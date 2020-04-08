b75bdd092fc5be429eb43512bd7ddec0b35855d1
Lys <thomas@lyseggen.xyz>
2020-04-08 21:46:33 +0200

    Add logic for rand org num

--- src/brreg.py
+++ src/brreg.py
@@ -4,12 +4,44 @@ import random
 from workflow import web
 from workflow import Workflow
 
+def calcChecksum(weights, num):
+    checksum = 0
+    for i in range(len(weights)):
+        checksum += weights[i] * int(num[i])
+    return checksum
+
+def validate(num):
+    base_11_weights = [2,3,4,5,6,7]
+    weights = []
+    for i in range(len(num) - 1):
+        j = i % len(base_11_weights)
+        weights.append(base_11_weights[j])
+    return (calcChecksum(weights, num) % 11) == 0
+
+def get_random_org():
+    num = ''
+    valid = False
+    while not valid:
+        num = ''
+        for i in range(9):
+            num += '' + str(int(random.random() * 10))
+        valid = validate(num)
+    return num
+
+
+
+
 def main(wf):
     user_input = ''.join(wf.args)
 
     if wf.update_available:
         wf.add_item("An update is available!",
                     autocomplete='workflow:update', valid=False)
+    if 'org' in user_input:
+        num = get_random_org()
+        wf.add_item('random org number', arg=num, valid=True)
+        wf.send_feedback()
+        return
 
     units = wf.cached_data('brreg', max_age=60*60*24) # It's not updating that often, so once a day is fine
     while units is None:
@@ -29,7 +61,7 @@ def main(wf):
             title = unit['navn']
             num = unit['organisasjonsnummer'] if 'organisasjonsnummer' in unit else -1
             if user_input.lower() in title.lower():
-                wf.add_item(title, arg=num, valid=(unit > 0))    
+                wf.add_item(title, arg=num, valid=(unit > 0))
     wf.send_feedback()
 
 
